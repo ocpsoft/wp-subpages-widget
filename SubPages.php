81f9bb85fbfbabedc9f2cfec3dd9e41ad21488f0
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-08-10 16:47:11 -0400

    Moved function into plugin (duh)

--- SubPages.php
+++ SubPages.php
@@ -42,6 +42,28 @@ function widget_subpages_init() {
 	register_widget_control(array('Sub Page Menu', 'widgets'), 'widget_subpages_control', 300, 190);
 }
 
+function the_subpages()
+{
+	global $post, $wpdb;
+
+	if ( is_page() )
+	{
+		if ( $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->posts WHERE post_type='page' AND post_parent = ".$post->ID) > 0 ){
+			$subpages = $post->ID;
+		}
+		else if ( $post->post_parent != 0 ){
+			$subpages = $post->post_parent;
+		}
+
+		if ($subpages)
+		{
+			echo '<ul>' . "\n";
+			wp_list_pages('title_li=&child_of='.$subpages);
+			echo '</ul>' . "\n";
+		}
+	}
+}
+
 // Run our code later in case this loads prior to any required plugins.
 add_action('widgets_init', 'widget_subpages_init');
 
